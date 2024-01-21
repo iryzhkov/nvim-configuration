2244a2b16ec37e8bb4a295422cab252fd12f66b0
Igor Ryzhkov <igor.o.ryzhkov@gmail.com>
2024-01-20 17:42:16 -0800

    Modified undotree config to show diff.

--- after/plugin/undotree.lua
+++ after/plugin/undotree.lua
@@ -1,6 +1,7 @@
-vim.g.undotree_DiffAutoOpen = 0
+vim.g.undotree_DiffAutoOpen = 1
 vim.g.undotree_HelpLine = 0
 vim.g.undotree_SetFocusWhenToggle = 1
+vim.g.undotree_WindowLayout = 2
 
 vim.keymap.set("n", "<leader><C-u>", vim.cmd.UndotreeFocus)
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
